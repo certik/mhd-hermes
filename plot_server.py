d332d139ac6063a0bda039d7463adfa7e7e96820
Ondrej Certik <ondrej@certik.cz>
2009-07-24 15:23:14 -0600

    Update the GUI asynchronously

--- plot_server.py
+++ plot_server.py
@@ -1,8 +1,15 @@
 print "Initializing..."
 import cPickle
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+from threading import Thread, Lock
+from time import sleep
+
+import wx
 from numpy import zeros
 from enthought.mayavi import mlab
+from enthought.pyface.api import GUI
+
+gui_lock = Lock()
 
 def plot(vert, triangles):
     """
@@ -24,15 +31,37 @@ def plot(vert, triangles):
     z = zeros(len(y))
     t = vert[:, 2]
     print "  done."
-    print "plotting the triangular mesh..."
-    s = mlab.triangular_mesh(x, y, z, triangles, scalars=t)
-    print "  done."
-    print "adjusting view..."
-    mlab.view(0, 0)
-    print "  done."
+    def doit():
+        print "plotting the triangular mesh..."
+        s = mlab.triangular_mesh(x, y, z, triangles, scalars=t)
+        print "  done."
+        print "adjusting view..."
+        mlab.view(0, 0)
+        print "  done."
+    gui_lock.acquire()
+    doit()
+    gui_lock.release()
 
-server = SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
-server.register_introspection_functions()
-server.register_function(plot)
-print "Listening on port 8000..."
-server.serve_forever()
+class Server(Thread):
+
+    def run(self):
+        server = SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
+        server.register_introspection_functions()
+        server.register_function(plot)
+        print "Listening on port 8000..."
+        server.serve_forever()
+
+server = Server()
+server.start()
+app = wx.GetApp()
+assert app is not None
+assert wx.Thread_IsMain()
+evtloop = wx.EventLoop()
+ea = wx.EventLoopActivator(evtloop)
+while 1:
+    gui_lock.acquire()
+    while evtloop.Pending():
+        evtloop.Dispatch()
+        app.ProcessIdle()
+    gui_lock.release()
+    sleep(0.001)
