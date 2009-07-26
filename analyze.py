594118d6f2b30b4adbf3b1d47286f12f14e96ec5
Ondrej Certik <ondrej@certik.cz>
2009-07-25 21:41:56 -0600

    upd

--- analyze.py
+++ analyze.py
@@ -7,21 +7,26 @@ y = load(f)
 z = load(f)
 triangles = load(f)
 t = load(f)
-print x
-print y
-print z
-print triangles
-print t
+#print x
+#print y
+#print z
+#print triangles
+#print t
 s = mlab.triangular_mesh(x, y, z, triangles, scalars=t)
 x = load(f)
 y = load(f)
 z = load(f)
 triangles2 = load(f)
 t = load(f)
-print x
-print y
-print z
+#print x
+#print y
+#print z
 print triangles
-print t
+print triangles2
+print len(triangles)
+print len(triangles2)
+# XXX: this works, but 2643 segfaults
+triangles = triangles2[:2642]
+#print t
 s.mlab_source.reset(x=x, y=y, z=z, triangles=triangles, scalars=t)
 mlab.show()
