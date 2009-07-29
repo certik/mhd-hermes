c51b6bc8810b54539c8e513dfd369de728feacb8
Ondrej Certik <ondrej@certik.cz>
2009-07-29 10:08:42 -0600

    Little polish

--- vis_vec.py
+++ vis_vec.py
@@ -67,8 +67,12 @@ def better():
 
 print "delaunay2d"
 field = mlab.pipeline.delaunay2d(vtk_file_reader)
+print "setting tolerance"
+#import IPython; IPython.embed()
+# this looks ugly:
+#field.filter.tolerance = 0.1065
 print "grid"
-x_g, y_g, z_g = numpy.mgrid[0:15:40j, 0:5:40j, 0:1:1j]
+x_g, y_g, z_g = numpy.mgrid[0:15:60j, 0:5:60j, 0:1:1j]
 print "scalar_scatter"
 sampling_grid = mlab.pipeline.scalar_scatter(x_g, y_g, z_g)
 
@@ -76,9 +80,13 @@ print "ProbeFilter"
 filter = mlab.pipeline.user_defined(sampling_grid, filter='ProbeFilter')
 print "outputs"
 filter.filter.source = field.outputs[0]
+# this doesn't look as good:
+#filter.filter.source = vtk_file_reader.outputs[0]
+#filter.filter.source = extract_vector_norm.outputs[0]
+#filter.filter.source = surface.outputs[0]
 
 print "vectors"
-vectors = mlab.pipeline.vectors(filter, mode='2darrow', scale_factor=1)
+vectors = mlab.pipeline.vectors(filter, mode='2darrow', scale_factor=0.1)
 print "done"
 
 vectors.glyph.color_mode = 'no_coloring'
@@ -86,5 +94,8 @@ vectors.actor.property.color = (0, 0, 0)
 #vectors.glyph.mask_input_points = True
 #vectors.glyph.mask_points.random_mode = False
 #vectors.glyph.mask_points.on_ratio = 200
+vectors.glyph.glyph.scale_factor = 0.5
+vectors.glyph.glyph_source.glyph_source.scale = 0.5
+vectors.glyph.glyph_source.glyph_source.center = array([0.25, 0, 0])
 
 mlab.show()
