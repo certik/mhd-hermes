62d0603553d4d0ea75460660804ecca8e99708b0
Ondrej Certik <ondrej@certik.cz>
2009-08-05 16:41:17 -0600

    Irregular obstacle

--- main.cpp
+++ main.cpp
@@ -115,7 +115,8 @@ int main(int argc, char* argv[])
 
   // a-priori mesh refinements
   mesh.refine_all_elements();
-  mesh.refine_towards_boundary(marker_obstacle, 4, false);
+  mesh.refine_all_elements();
+  mesh.refine_towards_boundary(marker_obstacle, 3, false);
   //mesh.refine_towards_boundary(marker_bottom, 4);
   //mesh.refine_towards_boundary(marker_top, 4);
   // plot the mesh:
